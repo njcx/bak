1998843cbde1dc5b97f3ae4fe928c51ceabd053c
nJcx <njcx86@gmail.com>
2018-01-31 17:52:28 +0800

    add blog

--- forms.py
+++ forms.py
@@ -4,19 +4,44 @@ from django.http import HttpResponseRedirect
 from .forms import NameForm
 
 def get_name(request):
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = NameForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
             return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = NameForm()
 
     return render(request, 'name.html', {'form': form})
+
+
+{% if form.subject.errors %}
+    <ol>
+    {% for error in form.subject.errors %}
+        <li><strong>{{ error|escape }}</strong></li>
+    {% endfor %}
+    </ol>
+{% endif %}
+
+
+{% for field in form %}
+    <div class="fieldWrapper">
+        {{ field.errors }}
+        {{ field.label_tag }} {{ field }}
+    </div>
+{% endfor %}
+
+
+{{ field.label_tag }}
+包含在HTML <label> 标签中的字段Label。它包含表单的label_suffix。例如，默认的label_suffix 是一个冒号：
+<label for="id_email">Email address:</label>
+
+
+>>> data = {'subject': 'hello',
+...         'message': 'Hi there',
+...         'sender': 'foo@example.com',
+...         'cc_myself': True}
+>>> f = ContactForm(data)
+>>> f.is_valid()
+True
+>>> f.cleaned_data
+{'cc_myself': True, 'message': 'Hi there', 'sender': 'foo@example.com', 'subject': 'hello'}
